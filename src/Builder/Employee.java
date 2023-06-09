9fc9d0a2364c361a4ca2723cf178723580b5a493
Manoj <hmanoj12@gmail.com>
2023-06-09 17:46:23 +0530

    Builder Design pattern by Builder lombok annotation

--- src/Builder/Employee.java
+++ src/Builder/Employee.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-
+@Builder
 
 public class Employee {
     private Integer empId;
@@ -13,53 +13,5 @@ public class Employee {
     private Double experience;
     private Double Salary;
 
-    //Constructor hiding
-    private  Employee(){
-
-    }
-
-    public static Builder builder(){
-        return new Builder();
-    }
-    public static class Builder extends Employee{
-        private Integer empId;
-        private String name;
-        private Department department;
-        private Double experience;
-
-
-        public Builder withEmpId(Integer empId){
-            this.empId = empId;
-            return this;
-        }
-
-        public Builder withName(String name){
-            this.name = name;
-            return this;
-        }
-
-        public Builder withDepartment(Department department){
-            this.department = department;
-            return this;
-        }
-
-        public Builder withExperience(Double experience){
-            this.experience = experience;
-            return this;
-        }
-
-
-
-        public Employee build(){
-            Employee employee = new Employee();
-            employee.empId = this.empId;
-            employee.name = this.name;
-            employee.department = this.department;
-            employee.experience = experience;
-            return employee;
-        }
-
-
-    }
 
 }
